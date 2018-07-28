1d73af048e3e47cb37acf5602d75031473c66edc
Yi Zu <zuyi@bu.edu>
2018-07-27 23:08:46 -0700

    On-demand submit form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function PlayerBio() {
+  $.ajax({
+    url:'/Person/Edit/@Model.Id/',
+    type:'post',
+    data: $('#playerbio').serialize(),
+    success() {
+      alert("worked");
+    }
+  });
+}
+
 module.exports = router;
