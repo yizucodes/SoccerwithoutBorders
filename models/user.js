c0d0957438632731f0e7bf199ccd532e3cc6e0ee
Yi Zu <zuyi@bu.edu>
2018-08-03 10:54:28 -0700

    Gitignore

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+
+// const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
@@ -12,38 +14,36 @@ const UserSchema = new Schema({
   // weight: { type: String, required: false },
 });
 
-UserSchema.pre('save', function (next) {
-  let user = this;
+// UserSchema.pre('save', function (next) {
+//   let user = this;
 
-  bcrypt.hash(user.password, 10, function (err, hash) {
-    if (err) return next(err);
+//   bcrypt.hash(user.password, 10, function (err, hash) {
+//     if (err) return next(err);
 
-    user.password = hash;
-    next();
-  })
-});
+//     user.password = hash;
+//     next();
+//   })
+// });
 
-UserSchema.statics.authenticate = function (username, password, next) {
-  User.findOne({ username: username })
-    .exec(function (err, user) {
-      if (err) {
-        return next(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return next(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return next(null, user);
-        } else {
-          return next();
-        }
-      });
-    });
-}
+// UserSchema.statics.authenticate = function (username, password, next) {
+//   User.findOne({ username: username })
+//     .exec(function (err, user) {
+//       if (err) {
+//         return next(err)
+//       } else if (!user) {
+//         var err = new Error('User not found.');
+//         err.status = 401;
+//         return next(err);
+//       }
+//       bcrypt.compare(password, user.password, function (err, result) {
+//         if (result === true) {
+//           return next(null, user);
+//         } else {
+//           return next();
+//         }
+//       });
+//     });
+// }
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
-
-
